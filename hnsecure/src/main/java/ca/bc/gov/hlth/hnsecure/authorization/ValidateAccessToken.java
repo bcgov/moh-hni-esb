9a28c7dee024bbc859ce10fff7415bc589b8ac4a
Pankaj Kathuria <pankaj.kathuria@cgi.com>
2021-04-22 12:38:38 -0600

    Updating as per review comments
    Updates for pull request https://github.com/bcgov/moh-hni-esb/pull/8

--- hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/authorization/ValidateAccessToken.java
+++ hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/authorization/ValidateAccessToken.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,76 +29,77 @@ import static ca.bc.gov.hlth.hnsecure.message.ErrorMessage.CustomError_Msg_Inval
 
 public class ValidateAccessToken implements Processor {
 
-    private static final Logger logger = LoggerFactory.getLogger(ValidateAccessToken.class);
+	private static final Logger logger = LoggerFactory.getLogger(ValidateAccessToken.class);
+	private static final String AUTH_HEADER_KEY = "Authorization";
 
-    private AuthorizationProperties authorizationProperties;
-    private String certsEndpoint;
-    private static String AUTH_HEADER_KEY = "Authorization";
+	private AuthorizationProperties authorizationProperties;
+	private String certsEndpoint;
 
-    /**
-     * Default Constructor
-     */
-    public ValidateAccessToken(AuthorizationProperties authorizationProperties, String certsEndpoint) {
-        this.authorizationProperties = authorizationProperties;
-        this.certsEndpoint = certsEndpoint;
-    }
 
-    @Override
-    public void process(Exchange exchange)
-            throws Exception {
-    	String methodName = "process";
-    	String authorizationKey = (String) exchange.getIn().getHeader(AUTH_HEADER_KEY);
-    	if(authorizationKey==null) {
-    		logger.info("{} - No authorization key passed in request header.", methodName);
-            throw new CustomHNSException(CustomError_Msg_InvalidAuthKey.getErrorMessage());
-    	}
-        AccessToken accessToken = AccessToken.parse(authorizationKey);
-        logger.info("{} - Access token: {}", methodName,accessToken);
+	/**
+	 * Default Constructor
+	 */
+	public ValidateAccessToken(AuthorizationProperties authorizationProperties, String certsEndpoint) {
+		this.authorizationProperties = authorizationProperties;
+		this.certsEndpoint = certsEndpoint;
+	}
 
-        // Create a JWT processor for the access tokens
-        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
-        jwtProcessor.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier(new JOSEObjectType("JWT")));
+	@Override
+	public void process(Exchange exchange)
+			throws Exception {
+		String methodName = "process";
+		String authorizationKey = (String) exchange.getIn().getHeader(AUTH_HEADER_KEY);
+		if(StringUtils.isBlank(authorizationKey)) {
+			logger.info("{} - No authorization key passed in request header.", methodName);
+			throw new CustomHNSException(CustomError_Msg_InvalidAuthKey.getErrorMessage());
+		}
+		AccessToken accessToken = AccessToken.parse(authorizationKey);
+		logger.info("{} - Access token: {}", methodName,accessToken);
 
-        // The public RSA keys to validate the signatures
-        // The RemoteJWKSet caches the retrieved keys to speed up subsequent look-ups
-        // TODO this should be moved into the constructor to make use of the JWK caching
-        JWSAlgorithm expectedJWSAlg = JWSAlgorithm.RS256;
-        JWKSource<SecurityContext> keySource = new RemoteJWKSet<>(
-                new URL(certsEndpoint),
-                // Overrides the DefaultResourceRetriever to up the timeouts to 5 seconds
-                new DefaultResourceRetriever(5000, 5000, 51200)
-        );
+		// Create a JWT processor for the access tokens
+		ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+		jwtProcessor.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier(new JOSEObjectType("JWT")));
 
-        // Configure the JWT processor with a key selector to feed matching public
-        // RSA keys sourced from the JWK set URL
-        JWSKeySelector<SecurityContext> keySelector = new JWSVerificationKeySelector<>(expectedJWSAlg, keySource);
-        jwtProcessor.setJWSKeySelector(keySelector);
+		// The public RSA keys to validate the signatures
+		// The RemoteJWKSet caches the retrieved keys to speed up subsequent look-ups
+		// TODO this should be moved into the constructor to make use of the JWK caching
+		JWSAlgorithm expectedJWSAlg = JWSAlgorithm.RS256;
+		JWKSource<SecurityContext> keySource = new RemoteJWKSet<>(
+				new URL(certsEndpoint),
+				// Overrides the DefaultResourceRetriever to up the timeouts to 5 seconds
+				new DefaultResourceRetriever(5000, 5000, 51200)
+				);
 
-        // Set the required JWT claims - these must all be available in the token payload
-        jwtProcessor.setJWTClaimsSetVerifier(
-                new CustomJWTClaimsVerifier(
-                        // Accepted Audience -> aud
-                        authorizationProperties.getAudiences(),
-                        // Accepted Authorized Parties -> azp
-                        authorizationProperties.getAuthorizedParties(),
-                        // Accepted Scopes -> scope
-                        authorizationProperties.getScopes(),
-                        // Exact Match Claims -> iss
-                        new JWTClaimsSet.Builder()
-                                .issuer(authorizationProperties.getIssuer())
-                                .build(),
-                        // Required Claims -> azp, scope, iat, exp, jti
-                        new HashSet<>(Arrays.asList("azp", "scope", "iat", "exp", "jti")),
-                        // Prohibited Claims
-                        null
-                )
-        );
+		// Configure the JWT processor with a key selector to feed matching public
+		// RSA keys sourced from the JWK set URL
+		JWSKeySelector<SecurityContext> keySelector = new JWSVerificationKeySelector<>(expectedJWSAlg, keySource);
+		jwtProcessor.setJWSKeySelector(keySelector);
 
-        // Process the token
-        JWTClaimsSet claimsSet = jwtProcessor.process(accessToken.toString(), null);
+		// Set the required JWT claims - these must all be available in the token payload
+		jwtProcessor.setJWTClaimsSetVerifier(
+				new CustomJWTClaimsVerifier(
+						// Accepted Audience -> aud
+						authorizationProperties.getAudiences(),
+						// Accepted Authorized Parties -> azp
+						authorizationProperties.getAuthorizedParties(),
+						// Accepted Scopes -> scope
+						authorizationProperties.getScopes(),
+						// Exact Match Claims -> iss
+						new JWTClaimsSet.Builder()
+						.issuer(authorizationProperties.getIssuer())
+						.build(),
+						// Required Claims -> azp, scope, iat, exp, jti
+						new HashSet<>(Arrays.asList("azp", "scope", "iat", "exp", "jti")),
+						// Prohibited Claims
+						null
+						)
+				);
 
-        // Print out the token claims set
-        logger.info("TOKEN PAYLOAD: " + claimsSet.toJSONObject());
-    }
+		// Process the token
+		JWTClaimsSet claimsSet = jwtProcessor.process(accessToken.toString(), null);
+
+		// Print out the token claims set
+		logger.info("{} - TOKEN PAYLOAD: {}", methodName, claimsSet.toJSONObject());
+	}
 
 }
