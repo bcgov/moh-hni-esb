558921ae8c2e2b08d23ad288ced01d42b68d125f
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2021-06-01 09:17:40 -0700

    Updated the error handling in hn-secure to provide more appropriate HTTP Status codes for specific errors (#19)
    * Updated the error handling in hn-secure to provide more appropriate HTTP Status codes for specific errors
    
    * Updated the error handling in hn-secure to provide more appropriate HTTP Status codes for specific errors

--- hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/messagevalidation/ExceptionHandler.java
+++ hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/messagevalidation/ExceptionHandler.java
@@ -1,31 +1,61 @@
 package ca.bc.gov.hlth.hnsecure.messagevalidation;
 
+import static ca.bc.gov.hlth.hnsecure.message.ErrorMessage.CustomError_Msg_InvalidAuthKey;
+import static ca.bc.gov.hlth.hnsecure.message.ErrorMessage.CustomError_Msg_InvalidRequest;
+import static ca.bc.gov.hlth.hnsecure.message.ErrorMessage.HL7Error_Msg_NoInputHL7;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
+import static org.apache.http.HttpStatus.SC_FORBIDDEN;
+import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR;
+import static org.apache.http.HttpStatus.SC_UNPROCESSABLE_ENTITY;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.http.conn.HttpHostConnectException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static ca.bc.gov.hlth.hnsecure.message.ErrorMessage.CustomError_Msg_InvalidRequest;;
+import ca.bc.gov.hlth.hncommon.util.LoggingUtil;
+import ca.bc.gov.hlth.hnsecure.exception.CustomHNSException;
+import ca.bc.gov.hlth.hnsecure.filedrops.FileDropGenerater;
 
 /**
- * Custom ExceptionHandler added to verify access token in request
- * @author pankaj.kathuria
+ * Custom ExceptionHandler to handle hn secure exceptions.
  *
  */
 public class ExceptionHandler implements Processor {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileDropGenerater.class);
 
     /**
-     * Validates the Hl7V2 transaction type (MSH.8) against the list of valid transaction type (valid-v2-message-types)
-     * @param v2Message the hl7v2 message to validate
-     */
-    
+     * Handle the exception thrown from the route. Based on the exception content it will add an
+     * appropriate HTTP Status Code.
+     * 
+     * @param exchange the exchange that is being processed
+     */    
 	@Override
-	public void process(Exchange exchange) throws Exception {
+	public void process(Exchange exchange) {
 
-        exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 403);
-        exchange.getIn().setBody("{ \"error\": \""+CustomError_Msg_InvalidRequest+"\" }");
-        return;
-    
+		Exception exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
 		
+		if (exception instanceof CustomHNSException) {
+			if (CustomError_Msg_InvalidAuthKey.getErrorMessage().equals(exception.getMessage())) {
+				exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, SC_FORBIDDEN);
+			} else if (CustomError_Msg_InvalidRequest.getErrorMessage().equals(exception.getMessage())){
+				exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, SC_BAD_REQUEST);
+			} else if (HL7Error_Msg_NoInputHL7.getErrorMessage().equals(exception.getMessage())){
+				exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, SC_UNPROCESSABLE_ENTITY);
+			} else {
+				exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, SC_BAD_REQUEST);
+			}
+		} else if (exception instanceof HttpHostConnectException) {
+			exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, SC_INTERNAL_SERVER_ERROR);
+			LOGGER.info("{} - Failed to connect remote server.",LoggingUtil.getMethodName());
+		} else {
+			// Should not reach here as the specific exception should be handled above, add default error in case the specific handling not added
+			exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, SC_INTERNAL_SERVER_ERROR);
+		}
+		
+		//Set the body to null as none is expected.
+        exchange.getIn().setBody(null);
 	}
-
 }
