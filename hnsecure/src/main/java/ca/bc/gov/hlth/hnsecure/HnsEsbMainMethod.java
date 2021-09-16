b5b6cf14044b87ebefad29aaba366b734a1792e2
Pankaj Kathuria <pankaj.kathuria@cgi.com>
2021-09-16 11:25:54 +1000

    Removing the dependency from env variables for local app server

--- hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/HnsEsbMainMethod.java
+++ hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/HnsEsbMainMethod.java
@@ -10,7 +10,8 @@ public final class HnsEsbMainMethod {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.setPropertyPlaceholderLocations(
-        		"file:${env:HNSESB_HOME}/properties/application-external.properties;optional=true,"
+        		"classpath:application-external.properties;optional=true,"
+        		+ "file:${env:HNSESB_HOME}/properties/application-external.properties;optional=true,"
         		+ "classpath:application.properties");
         main.configure().addRoutesBuilder(Route.class);
         main.run(args);
