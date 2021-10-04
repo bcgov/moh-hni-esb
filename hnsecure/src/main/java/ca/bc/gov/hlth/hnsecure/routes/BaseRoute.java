c73a0ab0c48c73e9c6dee1da4ff1b244e2ed6093
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2021-10-04 13:56:27 -0600

    Restored the overwritten replyToType

--- hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/routes/BaseRoute.java
+++ hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/routes/BaseRoute.java
@@ -28,7 +28,7 @@ public abstract class BaseRoute extends RouteBuilder {
 	 */
 	protected static final String JMS_DESTINATION_NAME_FORMAT = "queue:///%s?targetClient=1&&mdWriteEnabled=true";
 	
-	protected static final String MQ_URL_FORMAT = "jmsComponent:queue:%s?exchangePattern=InOut&replyTo=queue:///%s&replyToType=Exclusive&allowAdditionalHeaders=JMS_IBM_MQMD_MsgId&requestTimeout=7000&explicitQosEnabled=true&timeToLive=7000";
+	protected static final String MQ_URL_FORMAT = "jmsComponent:queue:%s?exchangePattern=InOut&replyTo=queue:///%s&replyToType=Shared&allowAdditionalHeaders=JMS_IBM_MQMD_MsgId&requestTimeout=7000&explicitQosEnabled=true&timeToLive=7000";
 	protected static final ApplicationProperties properties = ApplicationProperties.getInstance(); 
 
 	@SuppressWarnings("unchecked")
