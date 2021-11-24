8518fbf9f0a7a9ef036121bfea3b4f0f0dadcb09
Pankaj Kathuria <pankaj.kathuria@cgi.com>
2021-11-24 16:54:51 +1100

    updated methods to remove extra code

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -1,18 +1,20 @@
-// This groovy script is used for jenkins jobs
 
-def project(NAMESPACE) {
-	def ns = "${NAMESPACE}"
-	return "c5839f-" + ns.toLowerCase()
+#!groovy
+//-------------------------------------------------------------------
+// File: jenkins.groovy
+// Description: A util file for hnsesb jenkins jobs
+//-------------------------------------------------------------------
+
+def project(val) {
+	return "c5839f-" + val.toLowerCase()
 }
 
 def lower(val) {
-	def app = "${val}"
 	return val.toLowerCase()
 }
 
-def appName() {
-	def ns = "${NAMESPACE}"
-	return "hnsesb-" + ns.toLowerCase()
+def appName(val) {
+	return "hnsesb-" + val.toLowerCase()
 }
 
 def deployTag(DEPLOY_BUILD_NUMBER, DEPLOY_BUILD_TYPE){
@@ -23,26 +25,20 @@ def deployTag(DEPLOY_BUILD_NUMBER, DEPLOY_BUILD_TYPE){
 	return tag
 }
 // Method to verfiy pods 
-def verifyPods(CLUSTER, OC_JENKINS_USER, PROJECT, APP_NAME){
-	openshift.withCluster( "${CLUSTER}" ) {
-        openshift.withCredentials( "${OC_JENKINS_USER}" ) {
-            openshift.withProject("${PROJECT}"){
-				def pods =  openshift.selector( 'pods', [ app: '${APP_NAME}' ] )
-				// This will throw error, intended, if there are no pods with that application name
-				def podsObj = pods.objects()
-				// This loop will check if all pods are running status
-				pods.untilEach(1){
-					echo "Checking pod status"
-					def podObj = it.object()
-					def podStatus = podObj.status.phase
-					if('Running'=="${podStatus}")								    {
-						// Got to next pod status check
-						return true;
-					}else{
-						sh 'False'
-					}
-				}
-			}
+def verifyPods(val){
+	def pods =  openshift.selector( 'pods', [ app: val ] )
+	// This will throw error, intended, if there are no pods with that application name
+	def podsObj = pods.objects()
+	// This loop will check if all pods are running status
+	pods.untilEach(1){
+		echo "Checking pod status"
+		def podObj = it.object()
+		def podStatus = podObj.status.phase
+		if('Running'==podStatus)								    {
+			// Got to next pod status check
+			return true;
+		}else{
+			sh 'False'
 		}
 	}
 }
