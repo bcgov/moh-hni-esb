33cf502a1aa5d28420ba17010730ce765fa5f16e
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2022-03-21 17:33:43 -0600

    6050: removed check for msgType

--- hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/audit/AuditProcessor.java
+++ hnsecure/src/main/java/ca/bc/gov/hlth/hnsecure/audit/AuditProcessor.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +17,6 @@ import ca.bc.gov.hlth.hnsecure.audit.entities.TransactionEventType;
 import ca.bc.gov.hlth.hnsecure.audit.persistence.AbstractAuditPersistence;
 import ca.bc.gov.hlth.hnsecure.parsing.Util;
 import ca.bc.gov.hlth.hnsecure.parsing.V2MessageUtil;
-import ca.bc.gov.hlth.hnsecure.parsing.V2MessageUtil.MessageType;
 
 /**
  * Processor for logging auditing information.
@@ -26,66 +24,62 @@ import ca.bc.gov.hlth.hnsecure.parsing.V2MessageUtil.MessageType;
  */
 public class AuditProcessor extends AbstractAuditPersistence implements Processor {
 
-    private static Logger logger = LoggerFactory.getLogger(AuditProcessor.class);
-    
-    @Override
+	private static Logger logger = LoggerFactory.getLogger(AuditProcessor.class);
+
+	@Override
 	public void process(Exchange exchange) throws Exception {
 		String methodName = LoggingUtil.getMethodName();
 
-		TransactionEventType eventType = (TransactionEventType)exchange.getProperty(Util.PROPERTY_TRANSACTION_EVENT_TYPE);
+		TransactionEventType eventType = (TransactionEventType) exchange
+				.getProperty(Util.PROPERTY_TRANSACTION_EVENT_TYPE);
 		logger.debug("{} - Begin {}", methodName, eventType);
-		
+
 		// This assumes the processor is invoked via wiretap
-		String transactionId = (String)exchange.getProperty(Exchange.CORRELATION_ID);
-		Date eventTime = (Date)exchange.getProperty(Util.PROPERTY_TRANSACTION_EVENT_TIME);
-		
-		String v2Message = (String)exchange.getIn().getBody();
-		String msgType = V2MessageUtil.getMsgType(v2Message);
+		String transactionId = (String) exchange.getProperty(Exchange.CORRELATION_ID);
+		Date eventTime = (Date) exchange.getProperty(Util.PROPERTY_TRANSACTION_EVENT_TIME);
+
+		String v2Message = (String) exchange.getIn().getBody();
 		String messageId = null;
 		boolean logAffectedParties = false;
 		AffectedPartyDirection affectedPartyDirection = null;
-		
+
 		switch (eventType) {
 		case TRANSACTION_START:
-			
-			createTransactionAudit(exchange, transactionId, eventTime, v2Message);        
-	        //Affected Party - On transaction start log affected party info for R03, R09, R15, E45, R50        
-			if (!StringUtils.equals(msgType, MessageType.R09.name())) {
-				logAffectedParties = true;
-				affectedPartyDirection = AffectedPartyDirection.OUTBOUND;
-			}		
-			
-	        messageId = V2MessageUtil.getMsgId(v2Message);
+
+			createTransactionAudit(exchange, transactionId, eventTime, v2Message);
+			// Affected Party - On transaction start log affected party info for R03, R09,
+			// R15, E45, R50
+			logAffectedParties = true;
+			affectedPartyDirection = AffectedPartyDirection.OUTBOUND;
+			messageId = V2MessageUtil.getMsgId(v2Message);
 			break;
 		case MESSAGE_RECEIVED:
-	        
-			//Affected Party - On message received log affected party info for R09 as it's only available in the response.        
-			if (StringUtils.equals(msgType, MessageType.R09.name())) {
-				logAffectedParties = true;
-				affectedPartyDirection = AffectedPartyDirection.INBOUND;
-			}		
-	        messageId = V2MessageUtil.getMsgId(v2Message);
+
+			logAffectedParties = true;
+			affectedPartyDirection = AffectedPartyDirection.INBOUND;
+			messageId = V2MessageUtil.getMsgId(v2Message);
 			break;
 		default:
-			break;		
+			break;
 		}
 
-		TransactionEvent transactionEvent = createTransactionEvent(transactionId, eventType, eventTime, messageId);		
+		TransactionEvent transactionEvent = createTransactionEvent(transactionId, eventType, eventTime, messageId);
 		insert(transactionEvent);
-		
+
 		if (logAffectedParties) {
-			List<AffectedParty> affectedParties = createAffectedParties(v2Message, affectedPartyDirection, transactionId);
+			List<AffectedParty> affectedParties = createAffectedParties(v2Message, affectedPartyDirection,
+					transactionId);
 			if (!affectedParties.isEmpty()) {
 				insertList(affectedParties);
 			}
-		}		
-        logger.debug("{} - End {}", methodName, eventType);
+		}
+		logger.debug("{} - End {}", methodName, eventType);
 	}
 
 	private void createTransactionAudit(Exchange exchange, String transactionId, Date eventTime, String v2Message) {
 		String accessToken = (String) exchange.getIn().getHeader(Util.AUTHORIZATION);
 		String organizationId = Util.getSendingFacility(accessToken);
-		Transaction transaction = createTransaction(v2Message, transactionId, organizationId, eventTime);	        	
+		Transaction transaction = createTransaction(v2Message, transactionId, organizationId, eventTime);
 		insert(transaction);
 	}
 }
